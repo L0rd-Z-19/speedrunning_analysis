4b4c1e3e0ecd42827e61c69ab06c319015fbdf05
Ben Anderson <benjammin412@gmail.com>
2020-05-27 20:06:14 -0500

    updated Flask app

--- app.py
+++ app.py
@@ -1,37 +1,62 @@
-# Dependencies
-from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
-import python.py
+import numpy as np
 
-# Create an instance of Flask
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sql:///games.sql")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Game = Base.classes.game
+
+#################################################
+# Flask Setup
+#################################################
 app = Flask(__name__)
 
-# Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/speedrun_app")
-
-# Route to render index.html template using data from Mongo
 @app.route("/")
-def index():
+def games():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
 
-    # Find one record of data from the mongo database
-    data = mongo.db.collection.find_one()
+    """Return a list of passenger data including the name, age, and sex of each passenger"""
+    # Query all passengers
+    results = session.query(Game.Rank, Game.Name, Game.Platform, Game.Year, Game.Genre,
+    	Game.Publisher, Game.NA_Sales, Game.EU_Sales, JP_Sales, Other_Sales, Global_Sales).all()
 
-    # Return template and data
-    return render_template("index.html", speedrun=data)
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of all_passengers
+    all_games = []
+    for Rank, Name, Platform, Year, Genre, Publisher, NA_Sales,
+    EU_Sales, JP_Sales, Other_Sales, Global_Sales in results:
+        games_dict = {}
+        games_dict["Rank"] = Rank
+        games_dict["Name"] = Name
+        games_dict["Platform"] = Platform
+        games_dict["Year"] = Year
+        games_dict["Genre"] = Genre
+        games_dict["Publisher"] = Publisher
+        games_dict["NA_Sales"] = NA_Sales
+        games_dict["EU_Sales"] = EU_Sales
+        games_dict["JP_Sales"] = JP_Sales
+        games_dict["Other_Sales"] = Other_Sales
+        games_dict["Global_Sales"] = Global_Sales
+        all_games.append(games_dict)
+
+    return jsonify(all_games)
 
 
-# Route that will trigger the scrape function
-@app.route("/python")
-def scrape():
-
-    # Run the scrape function
-    run_data = python.scrape()
-
-    # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, run_data, upsert=True)
-
-    # Redirect back to home page
-    return redirect("/")
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
