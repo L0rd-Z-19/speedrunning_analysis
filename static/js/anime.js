a8dc4788d0e1ae973cb77aa5f39c8a90d14d054b
Zach <57845873+L0rd-Z-19@users.noreply.github.com>
2020-05-30 11:25:20 -0500

    MOVED AROUND FILES

--- static/js/anime.js
+++ static/js/anime.js
@@ -1,15 +1,3 @@
-// let timel = anime.timeline({
-//     easing: 'easeOutExpo',
-//     duration: 1000
-// });
-//     timel.add({
-//         targets: 'nav a h1',
-//         color: '#B9B9B9',
-//         delay: anime.stagger(100)
-//     })
-
-
-
 let tl = anime.timeline({
     easing: 'easeOutExpo',
     duration: 750
