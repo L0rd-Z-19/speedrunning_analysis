9c75cb3f0d1c391aa1b1ab87c6263869632e9b54
Zach <57845873+L0rd-Z-19@users.noreply.github.com>
2020-05-30 11:18:50 -0500

    completed the nav-bar

--- static/js/anime.js
+++ static/js/anime.js
@@ -1,12 +1,12 @@
-let timel = anime.timeline({
-    easing: 'easeOutExpo',
-    duration: 1000
-});
-    timel.add({
-        targets: 'nav a h1',
-        color: '#B9B9B9',
-        delay: anime.stagger(100)
-    })
+// let timel = anime.timeline({
+//     easing: 'easeOutExpo',
+//     duration: 1000
+// });
+//     timel.add({
+//         targets: 'nav a h1',
+//         color: '#B9B9B9',
+//         delay: anime.stagger(100)
+//     })
 
 
 
